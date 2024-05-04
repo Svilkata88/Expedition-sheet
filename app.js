d5b5a3c5c1e3d51b9291cd557de94e9365127dd8
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2024-05-04 20:27:22 +0300

    last commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const createButton = document.getElementById('create');
 const clearButton = document.getElementById('clear');
 const deleteButton = document.getElementById('delete');
-
+const confirmButton = document.getElementById('confirm-btn');
 
 createButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -13,7 +13,6 @@ createButton.addEventListener('click', (e) => {
     const palletTypeInput = document.getElementById('palletType');
     
     const tBodyElement = document.querySelector('tbody');
-
 
     const tdCode = document.createElement('td');
     tdCode.textContent = codeInput.value;
@@ -36,6 +35,11 @@ createButton.addEventListener('click', (e) => {
     const tdPalletType = document.createElement('td');
     tdPalletType.textContent = palletTypeInput.value;
 
+    const tdEdit = document.createElement('td')
+    tdEdit.type = 'submit';
+    tdEdit.name = 'редакция';
+    tdEdit.value = 'редакция';
+
     const tdCheckBox = document.createElement('td')
     tdCheckBox.className = 'check';
     const checkBoxInput = document.createElement('input');
@@ -47,7 +51,8 @@ createButton.addEventListener('click', (e) => {
     newRow.append(tdCode, tdQtty, tdWeight1Piece, tdTottalyWeight, tdPackage, tdNumberPackage, tdPalletType, tdPalletType, tdCheckBox)
 
     tBodyElement.appendChild(newRow);
-
+    createRowTotal();
+    clearInputs();
 
 })
 
@@ -61,8 +66,123 @@ deleteButton.addEventListener('click', (e) => {
             rowElement.remove();
         }
     })
-
-
-    //console.log(tbodyElement);
-    // console.log(checkedRows);
 })
+
+clearButton.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const tBodyElement = document.querySelector('tbody');
+    tBodyElement.innerHTML = '';
+})
+
+confirmButton.addEventListener('click', () => {
+    const confirmedSheet = {
+        totalQuantity: sumColumnTotal('thQtty'),
+        totalWeight: sumColumnTotal('thTotalWeight'),
+        totalPackage: sumColumnTotal('thPackage'), // ТРАБВА ДА СЕ РЕДАКТИРА!
+        totalNumberOfPallets: sumColumnTotal('thNumberOfPallets'),
+    }
+    const tBodyElement = document.querySelector('tbody');
+    tBodyElement.innerHTML = '';
+    console.log(confirmedSheet);
+})
+
+function clearInputs() {
+    const codeInput = document.getElementById('Code');
+    const qttyInput = document.getElementById('Qtty'); 
+    const packageInput = document.getElementById('package'); 
+    const numberPackageInput = document.getElementById('numberOfPallets');
+    const palletTypeInput = document.getElementById('palletType');
+
+    codeInput.value = '';
+    qttyInput.value = '';
+    packageInput.value = '';
+    numberPackageInput.value = '';
+    palletTypeInput.value = '';
+}
+
+function createRowTotal() {
+    const tBodyElement = document.querySelector('tbody');
+    const createdRows = tBodyElement.children;
+
+    if (createdRows.length > 1) {
+        const removedRow =  createdRows[createdRows.length - 1]
+        createdRows[createdRows.length - 1].remove()
+        const totalRow = createdRows[createdRows.length - 1]
+        createdRows[createdRows.length - 1].remove()
+        tBodyElement.appendChild(removedRow)
+        tBodyElement.appendChild(totalRow)
+
+        const tdQtty = totalRow.querySelector(`td:nth-child(${2})`);
+        tdQtty.textContent = sumColumnTotal('thQtty');
+
+        const tdTotalWeight = totalRow.querySelector(`td:nth-child(${4})`);
+        tdTotalWeight.textContent = sumColumnTotal('thTotalWeight');
+
+        const tdPackage = totalRow.querySelector(`td:nth-child(${5})`);
+        tdPackage.textContent = sumColumnTotal('thPackage');
+
+        const tdNumberOfPallets = totalRow.querySelector(`td:nth-child(${6})`);
+        tdNumberOfPallets.textContent = sumColumnTotal('thNumberOfPallets');
+        return
+    }
+
+    const tdCodeTotal = document.createElement('td');
+    tdCodeTotal.textContent = 'TОТАЛ';
+
+    const tdQttyTotal = document.createElement('td');
+    tdQttyTotal.textContent = '';
+
+    const tdWeight1PieceTotal = document.createElement('td');
+    tdWeight1PieceTotal.textContent = '';
+
+    const tdTottalyWeightTotal = document.createElement('td');
+    tdTottalyWeightTotal.textContent = '';
+
+    const tdPackageTotal = document.createElement('td');
+    tdPackageTotal.textContent = '';
+
+    const tdNumberPackageTotal = document.createElement('td');
+    tdNumberPackageTotal.textContent = '';
+
+    const tdPalletTypeTotal = document.createElement('td');
+    tdPalletTypeTotal.textContent = '';
+
+    const newRowTotal = document.createElement('tr');
+    newRowTotal.append(tdCodeTotal, tdQttyTotal, tdWeight1PieceTotal, tdTottalyWeightTotal, tdPackageTotal, tdNumberPackageTotal, tdPalletTypeTotal)
+
+    tBodyElement.appendChild(newRowTotal);
+
+    tdQttyTotal.textContent = sumColumnTotal('thQtty');
+    tdTottalyWeightTotal.textContent = sumColumnTotal('thTotalWeight');
+    tdPackageTotal.textContent = sumColumnTotal('thPackage');
+    tdNumberPackageTotal.textContent = sumColumnTotal('thNumberOfPallets');
+}
+
+function sumColumnTotal(columnidStr) {
+    // Find the column wich elements we want to sum
+    const col = document.getElementById(columnidStr);
+
+    //const tBodyElement = document.querySelector('tbody');
+
+    const nColumn = Number(col.getAttribute('n'));  
+    const qttyColumnElementsList = [];
+
+    // Get all tr elements within the tbody except the last one ( last one is TOTAL ROW where we sum)
+    const trElements = document.querySelectorAll('tbody tr:not(:last-child)');
+
+    // Iterate over each tr element
+    trElements.forEach(tr => {
+
+        // Get the nth child td element within each tr
+        const tdElement = tr.querySelector(`td:nth-child(${nColumn})`);
+
+        // Check if the td element exists
+        if (tdElement) {
+            // Add the td element to the list
+            qttyColumnElementsList.push(tdElement);
+        }
+    })
+    const result = qttyColumnElementsList.reduce((acc, element) => acc + Number(element.textContent) ,0);
+    return result;
+}
